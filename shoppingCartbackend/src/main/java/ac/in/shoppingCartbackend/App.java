7cc01a49fd592ebbe283dc567c626fb721d11f90
sathya0511 <sathyak470@gmail.com>
2018-12-30 11:23:49 +0530

    Project Started

---
+++ shoppingCartbackend/src/main/java/ac/in/shoppingCartbackend/App.java
@@ -0,0 +1,13 @@
+package ac.in.shoppingCartbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
